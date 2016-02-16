60cad8beacf5347c6af7ebdea64e349d206d6a29
etherdesign <etherdesign@etherdesign-PC>
2016-02-15 19:34:30 -0600

    SimUDuck Examples done for HFDP Chapter 1.

---
+++ src/simuduckch1/FlyRocketPowered.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package simuduckch1;
+
+/**
+ *
+ * @author etherdesign
+ */
+public class FlyRocketPowered implements FlyBehavior {
+    @Override
+    public void fly() {
+        System.out.println("I'm flying with a rocket!");
+    }
+}
